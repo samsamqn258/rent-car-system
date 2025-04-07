49dd857b1ef15828f1df3a7eba329dd01559be8e
huynhcuong27042003 <huynhminhcuong.270403@gmail.com>
2025-04-07 10:08:20 +0700

    update car

--- views/auth/login.php
+++ views/auth/login.php
@@ -22,6 +22,7 @@
                         </div>
                         
                         <div class="mb-3 form-check">
+                            
                             <input type="checkbox" class="form-check-input" id="remember" name="remember">
                             <label class="form-check-label" for="remember">Ghi nhớ đăng nhập</label>
                         </div>
